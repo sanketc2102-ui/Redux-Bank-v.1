62830ab08d4866166d9ff86a795a6a340058a286
Sanket Chavan <sanketc2102@gmail.com>
2026-06-30 13:47:40 +0530

    add store and dispach some action

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
+
+const store = createStore(reducer);
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -16,9 +20,10 @@ function reducer(state = initialState, action) {
       return {
         ...state,
         loan: state.loan + action.payload.amount,
+        balance: state.balance + action.payload.amount,
         loanPurpose: action.payload.purpose,
       };
-    case "account/payLoad":
+    case "account/payloan":
       return {
         balance: state.balance - state.loan,
         loan: 0,
@@ -29,3 +34,16 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+store.dispatch({ type: "account/deposite", payload: 500 });
+store.dispatch({ type: "account/withdraw", payload: 250 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 12500, purpose: "to buy new furniture" },
+});
+
+store.dispatch({ type: "account/payloan" });
+
+console.log(store.getState());
+
+export default store;
