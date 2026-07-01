a959ae542835cb64ba81d85996dcb482114c613b
Sanket Chavan <sanketc2102@gmail.com>
2026-07-01 12:40:39 +0530

    Read customer state fullanme and dispaly

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
 import Customer from "./features/customers/Customer";
 import CreateCustomer from "./features/customers/CreateCustomer";
+import { useSelector } from "react-redux";
 
 function App() {
+  const customerName = useSelector((store) => store.customer.fullName);
+
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+
+      {customerName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
