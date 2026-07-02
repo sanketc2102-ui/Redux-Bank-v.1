1b191c0a2043ed03c33e6c694c7b3917996a93d0
Sanket Chavan <sanketc2102@gmail.com>
2026-07-02 11:41:55 +0530

    impliment rtk way reducer

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,10 +1,13 @@
-const initialAccountState = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+/* 
 function accountReducer(state = initialAccountState, action) {
   switch (action.type) {
     case "account/deposite":
@@ -69,5 +72,67 @@ export function requestLoan(amount, purpose) {
 export function loanPaid() {
   return { type: "account/payloan" };
 }
+ */
 
-export default accountReducer;
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposite(state, action) {
+      state.balance += action.payload;
+    },
+
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+
+      reducer(state, action) {
+        state.balance += action.payload.amount;
+        state.loan += action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
+    },
+
+    loanPaid(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+console.log(accountSlice);
+
+export const { requestLoan, loanPaid, withdraw } = accountSlice.actions;
+
+export function deposite(amount, currency) {
+  if (currency === "USD") return { type: "account/deposite", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch("accounts/currencyConverting");
+
+    // API Call
+
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+
+    const {
+      rates: { USD },
+    } = await res.json();
+
+    dispatch({ type: "account/deposite", payload: USD });
+  };
+}
+
+export default accountSlice.reducer;
